b54ea2080c0e9c6c1e95d0b84844feb5bbcf2515
Sangwon1233git config --global user.name Sangwon1233git config --global user.name Your <tkddnjs3351@naver.com>
2025-06-08 15:26:25 +0900

    파이썬 구구단 만들기

--- test.py
+++ test.py
@@ -1,1 +1,12 @@
 print("ㅎㅇㅎㅇ")
+
+
+x=2 
+y=1
+print (x,'x',y,'=', x * y)
+
+for x in range(2,10):
+    for y in range(1,10):
+        print(x,'*',y,'=',x*y)
+        if(y == 9):
+            print(f'{x}단 끝')
